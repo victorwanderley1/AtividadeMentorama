847d70d5d233bf0f056dd209e29fac4d016004eb
Victor Wanderley <victor.wanderley1709@gmail.com>
2020-12-04 11:46:43 -0300

    Desenvolvendo Funçoes Da Classe EditarLista

--- src/atividadementorama/EditarLista.java
+++ src/atividadementorama/EditarLista.java
@@ -4,11 +4,17 @@
  * and open the template in the editor.
  */
 package atividadementorama;
-
+import java.util.ArrayList;
 /**
  *
  * @author Victor-Vanessa
  */
 public class EditarLista {
-    
+    private void adicionarCategoria(ArrayList<String> lista, String nomeCategoria){
+        lista.add(nomeCategoria);
+    }
+
+    private void removerCategoria(ArrayList<String> lista, int numeroDaCategoria){
+        lista.remove(numeroDaCategoria);
+    }
 }
